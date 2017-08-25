aaa8c8bba2fc76b184699a4adbeea5ea3ed5a468
Karen <K.Blanchette07@gmail.com>
2017-08-25 14:35:01 -0500

    Edited Server and Added Route to Home Page

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var PORT = process.env.PORT || 3000;
 
 var app = express();
 
+require("./controllers/ordersController")(app);
+
 app.listen(PORT, function() {
     console.log("Listening: " + PORT);
 })
