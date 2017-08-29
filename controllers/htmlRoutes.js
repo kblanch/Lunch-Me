36b9287c771e7d41baf329cfc7fe643805726ae1
Natalie <nathompson110@gmail.com>
2017-08-29 17:14:23 -0500

    Folder structure changes

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -3,18 +3,22 @@ var path = require("path");
 module.exports = function(app) {
 
   app.get("/adding", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/views/addOrderLine.html"));
+    res.sendFile(path.join(__dirname, "../public/addOrderLine.html"));
   });
 
   app.get("/confirming", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/views/confirmation.html"));
+    res.sendFile(path.join(__dirname, "../public/confirmation.html"));
   });
   app.get("/creating", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/views/creators_page.html"));
+    res.sendFile(path.join(__dirname, "../public/creators_page.html"));
   });
 
+    app.get("/", function(req, res){
+        // var message = "Login Page"
+        // res.send(message);
 
-  app.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/views/index.html"));
-  });
+        res.sendFile(path.join(__dirname, "../public/login.html"))
+      });
+
+  
 };
